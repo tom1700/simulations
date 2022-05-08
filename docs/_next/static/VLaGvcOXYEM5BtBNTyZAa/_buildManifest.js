b6eab6312363d7cb26a25f178f16315f1a521526
Tomasz Morawski <tomasz.morawski@hyperscience.com>
2022-05-08 12:46:12 +0200

    Add build project

--- docs/_next/static/VLaGvcOXYEM5BtBNTyZAa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/287-76c53959b8970ee9.js","static/chunks/pages/index-830d9be4474eba08.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
