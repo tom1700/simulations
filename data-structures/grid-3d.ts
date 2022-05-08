5254b83758282441884312343c50aa5997bf1afc
Tomasz Morawski <tomasz.morawski@hyperscience.com>
2022-05-08 11:45:51 +0200

    Implement rendering multiple cubes without optimizations

---
+++ data-structures/grid-3d.ts
@@ -0,0 +1,92 @@
+
+export const to1D = (x: number, y: number, z: number, width: number, height: number) => {
+    return (z * width * height) + (y * width) + x;
+}
+
+export const to3D = (idx: number, width: number, height: number) => {
+    const z = Math.floor(idx / (width * height));
+    idx -= (z * width * height);
+    const y = Math.floor(idx / width);
+    const x = idx % width;
+
+    return { x, y, z };
+}
+
+export enum Direction {
+    TOP = 'TOP', RIGHT = 'RIGHT', LEFT = 'LEFT', BOTTOM = 'BOTTOM', FRONT = 'FRONT', BACK = 'BACK'
+}
+
+export class Grid3D {
+    private _width: number;
+    private _height: number;
+    private _depth: number;
+    private buffer: Array<string>;
+    private neighbourFunctionMap: Record<Direction, (x: number, y: number, z: number) => number>
+
+    constructor(width: number, height: number, depth: number) {
+        this._width = width;
+        this._height = height;
+        this._depth = depth;
+        this.buffer = new Array(this.width * this.depth * this.height);
+        this.resetBuffer();
+
+        this.neighbourFunctionMap = {
+            [Direction.TOP]: (x: number, y: number, z: number) =>
+                to1D(x, y + 1, z, width, height),
+            [Direction.BOTTOM]: (x: number, y: number, z: number) =>
+                to1D(x, y - 1, z, width, height),
+            [Direction.BACK]: (x: number, y: number, z: number) =>
+                to1D(x, y, z - 1, width, height),
+            [Direction.FRONT]: (x: number, y: number, z: number) =>
+                to1D(x, y, z + 1, width, height),
+            [Direction.LEFT]: (x: number, y: number, z: number) =>
+                to1D(x - 1, y, z, width, height),
+            [Direction.RIGHT]: (x: number, y: number, z: number) =>
+                to1D(x + 1, y, z, width, height),
+        }
+    }
+
+    public resetBuffer() {
+        for (let i = 0; i < this.buffer.length; i++) {
+            this.buffer[i] = '';
+        }
+    }
+
+    public getNeighbourIndex(direction: Direction, x: number, y: number, z: number) {
+        return this.neighbourFunctionMap[direction](x, y, z);
+    }
+
+    public hasNeighbour = (direction: Direction, x: number, y: number, z: number) => {
+        const { buffer } = this;
+        const index = this.getNeighbourIndex(direction, x, y, z);
+
+        if (index < 0 || index > buffer.length) return false;
+
+        return !!buffer[index];
+    }
+
+    public setValue(x: number, y: number, z: number, value: string) {
+        const { width, height, depth } = this;
+        if (x < 0 || y < 0 || z < 0 || x >= width || y >= height || z >= depth) return;
+        this.buffer[to1D(x, y, z, width, height)] = value;
+    }
+
+
+    public getValue(x: number, y: number, z: number) {
+        const { width, height, depth } = this;
+        if (x < 0 || y < 0 || z < 0 || x >= width || y >= height || z >= depth) return undefined;
+        return this.buffer[to1D(x, y, z, width, height)];
+    }
+
+    public get width() {
+        return this._width;
+    }
+
+    public get height() {
+        return this._height;
+    }
+
+    public get depth() {
+        return this._depth;
+    }
+}
---
+++ pages/naive-cube.tsx
@@ -0,0 +1,146 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Container, Typography, Stack } from "@mui/material";
+import Slider from "@mui/material/Slider";
+import Button from "@mui/material/Button";
+import { useCallback, useRef, useState } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Grid3D } from "../data-structures/grid-3d";
+
+function resolutionLabelFormat(value: number) {
+  return value;
+}
+
+function calculateResolutionValue(value: number) {
+  return 2 ** (value - 8);
+}
+
+class Renderer3D {
+  private shouldStop = false;
+
+  startAnimation(
+    canvasElement: HTMLDivElement,
+    size: number,
+    resolution: number
+  ) {
+    const width = canvasElement.clientWidth;
+    const height = canvasElement.clientHeight;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+const meshMaterial = new THREE.MeshBasicMaterial({ color: 0xdd0000 });
+    new OrbitControls(camera, canvasElement);
+
+    camera.position.z = size * 3;
+    camera.position.y = size / 2;
+    camera.position.x = size / 2;
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    canvasElement.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(resolution, resolution, resolution);
+    const edges = new THREE.EdgesGeometry(geometry);
+
+    for (let x = 0; x < size / resolution; x += 1) {
+      for (let y = 0; y < size / resolution; y += 1) {
+        for (let z = 0; z < size / resolution; z += 1) {
+          const cubeLines = new THREE.LineSegments(edges, lineMaterial);
+          cubeLines.position.x = x * resolution;
+          cubeLines.position.y = y * resolution;
+          cubeLines.position.z = z * resolution;
+          scene.add(cubeLines);
+        }
+      }
+    }
+
+    const animate = () => {
+      if (this.shouldStop) {
+        canvasElement.removeChild(renderer.domElement);
+        return;
+      }
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+    animate();
+  }
+
+  stopAnimation() {
+    this.shouldStop = true;
+  }
+}
+
+const NaiveCube: NextPage = () => {
+  const [size, setSize] = useState(10);
+  const [resolution, setResolution] = useState(8);
+  const renderer3D = useRef<Renderer3D>();
+  const canvas = useRef<HTMLDivElement>(null);
+
+  const createCube = useCallback(() => {
+    if (renderer3D.current) {
+      renderer3D.current.stopAnimation();
+    }
+    if (!canvas.current) return;
+    renderer3D.current = new Renderer3D();
+    renderer3D.current.startAnimation(
+      canvas.current,
+      size,
+      calculateResolutionValue(resolution)
+    );
+  }, [resolution, size]);
+
+  return (
+    <Container
+      maxWidth="lg"
+      style={{
+        minHeight: "100vh",
+        paddingTop: "1rem",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Head>
+        <title>Simulations</title>
+        <meta name="description" content="Dynamic Cube Naive implementation" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Typography variant="h3" component="h1">
+        Dynamic Cube Naive implementation
+      </Typography>
+
+      <Stack spacing={2} direction="row" style={{ flex: 1 }}>
+        <div style={{ flex: 2 }} ref={canvas}></div>
+        <Stack spacing={2} style={{ flex: 1 }}>
+          Size: {size}
+          <Slider
+            min={1}
+            max={100}
+            step={1}
+            value={size}
+            onChange={(_, value) => !Array.isArray(value) && setSize(value)}
+          />
+          Resolution:{" "}
+          {resolutionLabelFormat(calculateResolutionValue(resolution))}
+          <Slider
+            scale={calculateResolutionValue}
+            value={resolution}
+            min={1}
+            step={1}
+            max={8}
+            valueLabelDisplay="auto"
+            onChange={(_, value) =>
+              !Array.isArray(value) && setResolution(value)
+            }
+          />
+          Total Grid Size:{" "}
+          {Math.pow(size / calculateResolutionValue(resolution), 3)}
+          <Button onClick={createCube}>Create</Button>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+};
+
+export default NaiveCube;
